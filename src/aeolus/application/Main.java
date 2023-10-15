e8f34311155a36e1a9640d79a9af546b7a515a2b
KlnSdr <monsieurk1209@gmail.com>
2023-10-15 22:13:20 +0200

    check if thot is running at startup

--- src/aeolus/application/Main.java
+++ src/aeolus/application/Main.java
@@ -1,9 +1,16 @@
 package aeolus.application;
 
 import dobby.Dobby;
+import dobby.util.logging.Logger;
+import thot.connector.Connector;
 
 public class Main {
     public static void main(String[] args) {
+        boolean thotInstanceRunning = Connector.write("testBucket", "isInstanceRunning", "true");
+        if (!thotInstanceRunning) {
+            new Logger(Main.class).error("Thot instance not running");
+            return;
+        }
         Dobby.startApplication(Main.class);
     }
 }
