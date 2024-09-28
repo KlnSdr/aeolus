9eb31e41376321cb6f675b0f0747c26190c5fdd6
KlnSdr <monsieurk1209@gmail.com>
2024-09-28 15:58:17 +0200

    calculate the correct color range for a temperature reading

--- src/aeolus/application/resource/static/blanket/index.ts
+++ src/aeolus/application/resource/static/blanket/index.ts
@@ -1,3 +1,18 @@
+const temperatureColors = [
+    { min: -Infinity, max: -6, cssVar: '--nNine' },
+    { min: -5.9, max: -3, cssVar: '--nSix' },
+    { min: -2.9, max: 0, cssVar: '--nThree' },
+    { min: 0, max: 2.9, cssVar: '--zero' },
+    { min: 3, max: 5.9, cssVar: '--three' },
+    { min: 6, max: 8.9, cssVar: '--six' },
+    { min: 9, max: 11.9, cssVar: '--nine' },
+    { min: 12, max: 14.9, cssVar: '--twelve' },
+    { min: 15, max: 17.9, cssVar: '--fifteen' },
+    { min: 18, max: 20.9, cssVar: '--eighteen' },
+    { min: 21, max: 23.9, cssVar: '--twentyOne' },
+    { min: 24, max: 26.9, cssVar: '--twentyFour' }
+];
+
 let monthData: dataPoint[] = [];
 let dayIndex: number = 0;
 
@@ -29,6 +44,7 @@ function loadNewData() {
         .then(data => {
             monthData = data;
             dayIndex = 0;
+            updateDisplayValue();
         })
         .catch(displayErrorPopUp);
 }
@@ -46,7 +62,12 @@ function loadMonthData(year: string, month: string): Promise<dataPoint[]> {
             if (processedData.length === 0) {
                 throw new Error("Keine Daten vorhanden");
             }
-            resolve(processedData);
+            resolve(processedData.map(day => {
+                return {
+                    date: day.date,
+                    value: parseFloat(day.value).toFixed(1) + " °C"
+                };
+            }));
         }).catch(e => {
             reject(e);
         });
@@ -86,4 +107,18 @@ function checkIndex() {
 function updateDisplayValue() {
     (document.getElementById("outTemperature") as HTMLLabelElement).innerText = monthData[dayIndex].value;
     (document.getElementById("outCurrentDate") as HTMLLabelElement).innerText = monthData[dayIndex].date;
+    changeOutputColor();
+}
+
+function changeOutputColor() {
+    const color: string = `var(${backgroundColorFromTemperature()})`;
+    (document.getElementById("outDayColor") as HTMLDivElement).style.backgroundColor = color;
+    (document.getElementById("outDayColor") as HTMLDivElement).style.boxShadow = `0 0 10px 5px ${color}`;
+}
+
+function backgroundColorFromTemperature(): string {
+    const temp: number = parseFloat(monthData[dayIndex].value);
+    const colorEntry = temperatureColors.find(entry => temp >= entry.min && temp <= entry.max);
+
+    return colorEntry ? colorEntry.cssVar : '--transparent';
 }
