71d814b06d5c002b3aeb54855b0a6f94792c7f2c
KlnSdr <monsieurk1209@gmail.com>
2024-09-28 19:11:20 +0200

    render blanket for a fixed year

--- src/aeolus/application/resource/static/blanket/blanketPreviewSketch.ts
+++ src/aeolus/application/resource/static/blanket/blanketPreviewSketch.ts
@@ -1,3 +1,19 @@
+// NOTE keep in sync with blanket/style.css
+const temperatureColorsP5 = [
+    { min: -Infinity, max: -6, color: '#3c224a' },
+    { min: -5.9, max: -3, color: '#232040' },
+    { min: -2.9, max: 0, color: '#84d0d9' },
+    { min: 0, max: 2.9, color: '#3bebd3' },
+    { min: 3, max: 5.9, color: '#184a5c' },
+    { min: 6, max: 8.9, color: '#00300f' },
+    { min: 9, max: 11.9, color: '#134f07' },
+    { min: 12, max: 14.9, color: '#769400' },
+    { min: 15, max: 17.9, color: '#fc9292' },
+    { min: 18, max: 20.9, color: '#ab5824' },
+    { min: 21, max: 23.9, color: '#a82c07' },
+    { min: 24, max: Infinity, color: '#571000' }
+];
+
 let yearData: dataPoint[] = [];
 let p5DidError: boolean = false;
 
@@ -39,8 +55,15 @@ const sketch = function(p: any) {
             return;
         }
 
+        const barWidth = p.width / yearData.length;
+        p.noStroke();
+
         yearData.forEach((dataPoint, index) => {
-            console.log(dataPoint);
+            const temp: number = parseFloat(dataPoint.value);
+            const colorEntry = temperatureColorsP5.find(entry => temp >= entry.min && temp <= entry.max);
+            p.fill(colorEntry ? colorEntry.color : 0);
+            p.rect(index * barWidth, 0, barWidth, p.height);
         });
+        p.noLoop();
     };
 };
