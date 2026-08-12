26a073f039925c7f648b31a235e8491c1e4e1aaa
KlnSdr <monsieurk1209@gmail.com>
2026-08-12 15:39:58 +0200

    implement mobile navbar

--- src/aeolus/application/resource/frontend/src/NavBar.elm
+++ src/aeolus/application/resource/frontend/src/NavBar.elm
@@ -2,8 +2,11 @@ module NavBar exposing (Msg(..), navBar, unauthNavBar)
 
 import CommonStyles exposing (buttonStyle)
 import Css exposing (..)
-import Html.Styled exposing (Html, a, button, div, h1, label, nav, text)
-import Html.Styled.Attributes exposing (css, href)
+import Css.Global
+import Css.Media as Media exposing (only, screen, withMedia)
+import FeatherIcons
+import Html.Styled exposing (Html, a, button, div, fromUnstyled, h1, input, label, nav, text)
+import Html.Styled.Attributes as Attributes exposing (css, for, href, id, type_)
 import Html.Styled.Events exposing (onClick)
 import List exposing (length)
 import Messages exposing (Message)
@@ -58,18 +61,48 @@ userToName user =
             "..."
 
 
+navToggleId : String
+navToggleId =
+    "nav-toggle"
+
+
+navToggleCheckbox : Html Msg
+navToggleCheckbox =
+    input
+        [ type_ "checkbox"
+        , id navToggleId
+        , Attributes.class "nav-toggle-checkbox"
+        , css navToggleCheckboxStyle
+        ]
+        []
+
+
+hamburgerButton : Html Msg
+hamburgerButton =
+    label
+        [ for navToggleId
+        , css hamburgerButtonStyle
+        , Attributes.attribute "aria-label" "Menü öffnen"
+        ]
+        [ FeatherIcons.menu |> FeatherIcons.withSize 22 |> FeatherIcons.toHtml [] |> fromUnstyled ]
+
+
 navBar : Maybe User -> List Message -> Html Msg
 navBar user messages =
     nav
         [ css navBarStyle
         ]
-        [ h1 [] [ a [ css navHeadingStyle, href "/" ] [ text "Aeolus" ] ]
-        , div []
-            [ div [ css navBarButtonsStyle ] (List.map (\e -> button [ css buttonStyle, onClick (NavElementClicked e.location) ] [ text e.displayText ]) navBarElements)
-            , div [ css navBarRight ]
-                [ button [ css buttonStyle, onClick MessagesClicked ] [ text ("Nachrichten: " ++ (messages |> length |> fromInt)) ]
-                , label [] [ text ("(" ++ userToName user ++ ")") ]
-                , button [ css buttonStyle, onClick LogoutClicked ] [ text "abmelden" ]
+        [ div [ css navHeaderRowStyle ]
+            [ h1 [] [ a [ css navHeadingStyle, href "/" ] [ text "Aeolus" ] ]
+            , hamburgerButton
+            ]
+        , navToggleCheckbox
+        , div [ Attributes.class "nav-menu", css navMenuStyle ]
+            [ div [ css navBarButtonsStyle ] (List.map (\e -> button [ css (buttonStyle ++ navButtonExtraStyle), onClick (NavElementClicked e.location) ] [ text e.displayText ]) navBarElements)
+            , div [ css navBarRightStyle ]
+                [ button [ css (buttonStyle ++ navButtonExtraStyle), onClick MessagesClicked ] [ text ("Nachrichten: " ++ (messages |> length |> fromInt)) ]
+                , label [ css navUserLabelStyle ] [ text ("(" ++ userToName user ++ ")") ]
+                , button [ css (buttonStyle ++ navButtonExtraStyle), onClick LogoutClicked ] [ text "abmelden" ]
                 ]
             ]
         ]
@@ -80,23 +113,84 @@ unauthNavBar =
     nav
         [ css navBarStyle
         ]
-        [ h1 [] [ a [ css navHeadingStyle, href "/" ] [ text "Aeolus" ] ]
-        , div []
-            [ div [ css navBarButtonsStyle ] []
-            , div [ css navBarRight ]
-                [ button [ css buttonStyle, onClick LoginClicked ] [ text "anmelden" ]
-                ]
+        [ div [ css navHeaderRowStyle ]
+            [ h1 [] [ a [ css navHeadingStyle, href "/" ] [ text "Aeolus" ] ]
+            ]
+        , div [ css navBarRightStyle ]
+            [ button [ css (buttonStyle ++ navButtonExtraStyle), onClick LoginClicked ] [ text "anmelden" ]
             ]
         ]
+
+
+mobileBreakpoint : Float
+mobileBreakpoint =
+    960
+
+
+mobileMediaQuery : List Style -> Style
+mobileMediaQuery =
+    withMedia [ only screen [ Media.maxWidth (px mobileBreakpoint) ] ]
 
 
 navBarStyle : List Css.Style
 navBarStyle =
     [ backgroundColor (hex "#00008b")
     , color (hex "#f9f9f9")
-    , height (px 50)
+    , position relative
     , displayFlex
     , alignItems center
+    , height (px 50)
+    , mobileMediaQuery
+        [ flexDirection column
+        , alignItems stretch
+        , height auto
+        ]
+    ]
+
+
+navHeaderRowStyle : List Style
+navHeaderRowStyle =
+    [ displayFlex
+    , alignItems center
+    , justifyContent spaceBetween
+    , mobileMediaQuery
+        [ width (pct 100)
+        , boxSizing borderBox
+        , padding2 (px 4) (px 10)
+        ]
+    ]
+
+
+hamburgerButtonStyle : List Style
+hamburgerButtonStyle =
+    [ display none
+    , cursor pointer
+    , color (hex "#f9f9f9")
+    , padding (px 8)
+    , mobileMediaQuery
+        [ displayFlex
+        , alignItems center
+        ]
+    ]
+
+
+navToggleCheckboxStyle : List Style
+navToggleCheckboxStyle =
+    [ position absolute
+    , opacity (num 0)
+    , width (px 1)
+    , height (px 1)
+    , mobileMediaQuery
+        [ checked
+            [ Css.Global.generalSiblings
+                [ Css.Global.class "nav-menu"
+                    [ displayFlex
+                    , flexDirection column
+                    , alignItems stretch
+                    ]
+                ]
+            ]
+        ]
     ]
 
 
@@ -112,17 +206,70 @@ navHeadingStyle =
     ]
 
 
-navBarButtonsStyle : List Style
-navBarButtonsStyle =
-    [ display inlineBlock
-    , margin2 (px 0) (px 10)
+navMenuStyle : List Style
+navMenuStyle =
+    [ displayFlex
+    , alignItems center
+    , justifyContent spaceBetween
+    , flexGrow (num 1)
+    , mobileMediaQuery
+        [ display none
+        , flexDirection column
+        , alignItems stretch
+        , width (pct 100)
+        ]
     ]
 
 
-navBarRight : List Style
-navBarRight =
-    [ position absolute
-    , top (px 0)
-    , right (px 0)
+navBarButtonsStyle : List Style
+navBarButtonsStyle =
+    [ displayFlex
+    , alignItems center
+    , margin2 (px 0) (px 10)
+    , mobileMediaQuery
+        [ flexDirection column
+        , alignItems stretch
+        , margin (px 0)
+        , width (pct 100)
+        ]
+    ]
+
+
+navButtonExtraStyle : List Style
+navButtonExtraStyle =
+    [ mobileMediaQuery
+        [ width (pct 100)
+        , minHeight (px 44)
+        , padding2 (px 10) (px 10)
+        , fontSize (px 16)
+        , textAlign left
+        , boxSizing borderBox
+        ]
+    ]
+
+
+navBarRightStyle : List Style
+navBarRightStyle =
+    [ displayFlex
+    , alignItems center
     , margin (px 12.5)
+    , marginLeft auto
+    , mobileMediaQuery
+        [ flexDirection column
+        , alignItems stretch
+        , margin (px 0)
+        , padding2 (px 8) (px 10)
+        , width (pct 100)
+        , boxSizing borderBox
+        ]
+    ]
+
+
+navUserLabelStyle : List Style
+navUserLabelStyle =
+    [ margin2 (px 0) (px 10)
+    , mobileMediaQuery
+        [ margin2 (px 8) (px 0)
+        , textAlign left
+        ]
     ]
